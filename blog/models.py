6e81536369adea1334590562811edb9e7fe931a0
Sergei Dmitriev <sergei.dmitriev@vk.team>
2024-02-15 20:41:54 +0300

    Auto commit

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from mdeditor.fields import MDTextField
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    body = models.TextField()
+    body = MDTextField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
