86e526f9eb0cca35bf847c0b6a17b9d61ccdc396
Sergei Dmitriev <sergei.dmitriev@vk.team>
2024-02-16 18:01:11 +0300

    Auto commit

--- sdmitriev/urls.py
+++ sdmitriev/urls.py
@@ -18,13 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from django.con import settins
+from django.conf import settings
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('pages.urls')),
     path("blog/", include("blog.urls")),
     url('meditor/', include('meditor.urls')),
+    "robots.txt", TemplateView.as_view(template_name="robots/txt",
+                                       content_type="text/plain")),
 ]
 
 if settings.DEBUG:
